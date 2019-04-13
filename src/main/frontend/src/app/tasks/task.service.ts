36bba957ab2ac9e0d48023effb35f339c185b0e9
KarMen <karolmendza@gmail.com>
2019-04-13 20:53:14 +0200

    Added call to save data

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,5 +1,6 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {Task} from "./task.model";
 
 @Injectable()
 export class TaskService {
@@ -10,4 +11,12 @@ export class TaskService {
     getTasks() {
         return this.httpClient.get('/api/tasks');
     }
+
+    saveTask(task: Task, checked: boolean) {
+        task.completed = checked;
+        return this.httpClient.post('/api/tasks/save', task).forEach(
+            (response: Response) => {return response.json()}
+        );
+    }
+
 }
--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -29,6 +29,6 @@ export class TasksListComponent implements OnInit {
     }
 
     onTaskChange(event, task) {
-        console.log("Task has changed");
+        this.taskService.saveTask(task, event.target.checed);
     }
 }
